9ace100cb431e3aa7863ca639f11bd0ec6dafe82
余浩 <980187223@qq.com>
2020-06-16 15:44:27 +0800

    添加支付Servlet

--- src/main/java/cn/tedu/PayServlet.java
+++ src/main/java/cn/tedu/PayServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -18,5 +19,14 @@ public class PayServlet extends HttpServlet {
         response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
         //----------------------------------------------------------
+        //1.获取一个session对象(获取之前的session)
+        HttpSession session = request.getSession();
+        String prod = (String) session.getAttribute("cart");
+        //3.模拟支付，做出响应
+        if(prod==null){
+            out.write("没有添加任何商品");
+        }else {
+            out.write("成功为"+prod+"完成支付");
+        }
     }
 }
